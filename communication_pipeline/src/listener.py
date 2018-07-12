422595f8b8af59e9ea0fd24c2fff697d68d82dca
bwsiuav-flying-squirrel-1 <bwsiuav+flying-squirrel@gmail.com>
2018-07-12 13:45:54 -0400

    tlk+lst

---
+++ communication_pipeline/src/listener.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from __future__ import division, print_function
+
+import rospy
+from std_msgs.msg import String
+
+'''
+A python script to practice receiving ROS messages
+'''
+
+class Listener():
+    ''' Subscribes to ROS messages
+    '''
+    def __init__(self):
+
+        # subscription objects
+        self.chatter_sub = rospy.Subscriber("/chatter", String, self.chatter_callback)
+
+
+    def chatter_callback(self, msg):
+        ''' Function to be run everytime a message is received on chatter topic
+        '''
+	msg =  "this is a listern"
+	return msg
+
+        '''TODO-START: FILL IN CODE HERE 
+        * print out the message received to the terminal
+        '''
+
+if __name__ == '__main__':
+    '''
+    This is where the code starts running
+    '''
+    rospy.init_node('listener')
+    l_obj = Listener()
+    print("Listener node running")
+    rospy.spin()
