fbb657c5a00d5a5cf1e6c9a67a47d7b724595781
Albert Farah <arfarah2002@gmail.com>
2018-07-23 15:53:50 -0400

    hi

--- open_loop_control/src/square.py
+++ open_loop_control/src/square.py
@@ -1,34 +1,32 @@
-from transition_control import *
+import translation_control as tc
 
 if __name__ == '__main__':
-    controller = TranslationController(MANEUVER_VELOCITY_SETPOINT, MANEUVER_REFERENCE_FRAME, MANEUVER_DURATION)
+    controller = tc.TranslationController(tc.MANEUVER_VELOCITY_SETPOINT, tc.MANEUVER_REFERENCE_FRAME, tc.MANEUVER_DURATION)
+fin = 'bu'               	#
+velsp__fin = []
+duration = []
 
-	    #Square ######################
-	fin = 'bu'               	#
-    velsp__fin = []
-    duration = []
+velsp__fin.append([0,1, 0])  #
+duration.append(2)       	#
+                         	#
+velsp__fin.append([0,0,1])   #
+duration.append(2)       	#
+                         	#
+velsp__fin.append([0,0,1])   #
+duration.append(2)       	#
 
-	velsp__fin.append([0,1, 0])  #
-	duration.append(2)       	#
-	                         	#
-	velsp__fin.append([0,0,1])   #
-	duration.append(2)       	#
-	                         	#
-	velsp__fin.append([0,0,1])   #
-	duration.append(2)       	#
+velsp__fin.append([0,-1,0])
+duration.append(2)
 
-	velsp__fin.append([0,-1,0])
-	duration.append(2)
+velsp__fin.append([0,0,-1])   #
+duration.append(2)        	#
+###############################
 
-	velsp__fin.append([0,0,-1])   #
-	duration.append(2)        	#
-	###############################
+    # Iterate through maneuvers
+for i in range(len(steps)):
+    controller.execute_maneuver(velsp__fin[i], fin, duration[i])
 
-	    # Iterate through maneuvers
-    for i in range(len(steps)):
-        execute_maneuver(velsp__fin[i], fin, duration[i])
+rospy.spin()
 
-    rospy.spin()
-
-    controller.stop_streaming_offboard_points()
-    print('DONE!')
+controller.stop_streaming_offboard_points()
+print('DONE!')
