33f9ebb705f5ed3bde77ddc2e51906578fdd47f6
Devon Minor <bwsiuav+flying-squirrel@gmail.com>
2018-07-24 09:12:54 -0400

    fix

--- open_loop_control/src/translation_control_copy.py
+++ open_loop_control/src/translation_control_copy.py
@@ -258,7 +258,7 @@ class TranslationController:
         self.vel_setpoint_bu_lenu__lenu = Twist()
     
 
-    square = [np.array([0,1,0]), np.array([0,0,1]), np.array([0,0,1]), np.array([0,-1,0]), np.array([0,0,-1])]
+    square = [np.array([0,1,0]), np.array([0,0,1]), np.array([0,-1,0]), np.array([0,0,-1])]
 
     def square(self):
     	for i in square:
