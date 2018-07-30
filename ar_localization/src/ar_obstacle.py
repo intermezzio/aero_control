6cd38a70905d36e318c23ccfca05755e11b37bac
Albert Farah <arfarah2002@gmail.com>
2018-07-30 14:05:13 -0400

    hi

--- ar_localization/src/ar_obstacle.py
+++ ar_localization/src/ar_obstacle.py
@@ -117,7 +117,8 @@ class ARObstacleController:
 ###########################################################################################################################
 # TODO: filter your detections for the best marker you can see (think about useful metrics here!)
 ###########################################################################################################################
-        	self.current_obstacle_tag = min(self.markers, key= marker.pose.pose.position.x)
+        	self.current_obstacle_tag = min(self.markers, key= marker.pose.pose.position.x).id
+        return
 
             
     def generate_vel(self): # assesses course of action using finite states
