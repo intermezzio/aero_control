e6ec96dbf16c26626630d42370306d2f30d12868
intermezzio <amascillaro@gmail.com>
2018-07-31 09:40:35 -0400

    Edit startdrone.sh

--- startdrone.sh
+++ startdrone.sh
@@ -1,6 +1,6 @@
 roscore &
 cd ~/bwsi-uav/catkin_ws/src/aero-control/nice_on
-% play NiceOn1.wav
+# play NiceOn1.wav
  
 sudo systemctl start aero-teraranger.service
 cd ~/bwsi-uav/catkin_ws/src/aero-optical-flow/build
@@ -8,4 +8,4 @@ sudo -E ./aero-optical-flow
 cd ~/bwsi-uav/catkin_ws
 source devel/setup.bash
 cd ~/bwsi-uav/catkin_ws/src/aero-control/nice_on
-% play NiceApproach1.wav
+# play NiceApproach1.wav
