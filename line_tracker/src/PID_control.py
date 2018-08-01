dc1589cb7a4ff3848f1019080c44722b9de1bbbf
intermezzio <amascillaro@gmail.com>
2018-08-01 14:33:56 -0400

    Fix merge for PID_Control

--- line_tracker/src/PID_control.py
+++ line_tracker/src/PID_control.py
@@ -46,16 +46,8 @@ class PIDController:
         return newcmd
         # newError +=
 if __name__ == "__main__":
-<<<<<<< HEAD
-    pid = PIDController(kp=0.75)
-    pid += 10
-    print bool(pid)
-    # print pid.errors
-    # print pid.adjust()
-=======
     pid = PIDController()
     pid.append(10)
     print "\n\n\nwow\n\n\n" if pid else "\n\n\rfalsse\n\n\n"
     # print pid.errors
     print pid.adjust()
->>>>>>> c0612f5daba0c422f75edb9f5c71c79194382604
