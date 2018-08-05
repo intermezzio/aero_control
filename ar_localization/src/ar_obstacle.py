8d1f46095e0867cabf9400a38c478db5ee17625f
Albert Farah <arfarah2002@gmail.com>
2018-08-04 20:21:08 -0400

    haha removing time didn't work

--- ar_localization/src/ar_obstacle.py
+++ ar_localization/src/ar_obstacle.py
@@ -140,16 +140,16 @@ class ARObstacleController:
 			net_pos = _CLEARANCE - curr_pos # how far we need to go: _CLEARANCE meters above
 			if -curr_pos < 0.75:
 				rospy.loginfo("FLY UP")
-			#time.sleep(1)
-			#z_vel = self.finite_state = 0
+			time.sleep(1)
+			self.finite_state = 0
 		elif self.finite_state == 3:
 			rospy.loginfo("avoiding gate")
 			curr_pos = self.current_obstacle_marker.pose.pose.position.z # position of current tag
 			net_pos = - _CLEARANCE - curr_pos # how far we need to go: _CLEARANCE meters above
 			if -curr_pos > -0.75:
 				rospy.loginfo("FLY DOWN")
-			#time.sleep(3)
-			#z_vel = self.finite_state = 0
+			time.sleep(1)
+			self.finite_state = 0
 
 		if abs(net_pos) < _THRESH:
 			rospy.loginfo("We're in range!")
